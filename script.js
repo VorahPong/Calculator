24b1307c95a50060875026910c42f29ecaacbf0b
VorahPong <vorahpongmean@gmail.com>
2024-08-11 03:11:52 -0500

    Added operate function
    the function take 2 numbers and 1 operator symbol then it will return
    the result.

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ let num2 = 0;
 let operator = '';
 //
 
+// Operate function
+function operate(num1, num2, operate){
+    switch(operate){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            throw new Error("ERROR!!");
+    }
+}
+
